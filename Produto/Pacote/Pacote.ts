b2b9b0ebdc66d4650f2ca4e2974bf6c9b4023b35
pgdesigner <39765554+pgdesigner@users.noreply.github.com>
2018-06-11 20:04:14 -0300

    Alterações em pacotes
    Fiz a parte de 15% dos preços

--- Produto/Pacote/Pacote.ts
+++ Produto/Pacote/Pacote.ts
@@ -1,42 +1,42 @@
 import { ProdutoAbstract } from '../ProdutoAbstract';
-import {Data} from '../../Data/Data';
+import {Data} from './Data/Data';
 
 export class Pacote extends ProdutoAbstract{
     private produtos: Array<ProdutoAbstract>;
     private validade: Data;
     private precoMensal: number;
 
-    public constructor(codigo: string, nome: string, genero: string, 
-                preco: number, produtos: Array<ProdutoAbstract>,
-                validade: Data, precoMensal: number){
+    public constructor(codigo: string, nome: string, genero: string, produtos: Array<ProdutoAbstract>,
+                validade: Data){
         
-        super(codigo, nome, genero, preco)
+        super(codigo, nome, genero, 0);
         this.produtos = produtos;
         this.validade = validade;
-        this.precoMensal = precoMensal;
+        let p:number=0;
+        for(let a of this.produtos){
+            p+=a.getPreco();
+        }
+        this.preco = p;
+        this.precoMensal = this.preco*0.15;
     }
-
     public getProdutos(): Array<ProdutoAbstract>{
         return this.produtos;
     }
     public setProdutos(produtos: Array<ProdutoAbstract>): void{
         this.produtos = produtos;
     }
-
     public getValidade(): Data{
         return this.validade;
     }
     public setValidade(validade: Data): void{
         this.validade = validade;
     }
-
     public getPrecoMensal(): number{
         return this.precoMensal
     }
     public setPrecoMensal(precoMensal: number): void{
         this.precoMensal = precoMensal;    
     }
-
     //public somaPrecoProduto() : void{
     //    for(let i
 
