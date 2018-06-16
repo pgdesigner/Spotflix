4f17465ef59ccee7046a5f0dc73ff186a18a1352
pgdesigner <39765554+pgdesigner@users.noreply.github.com>
2018-06-16 18:15:35 -0300

    a

--- Interface/Interface.ts
+++ Interface/Interface.ts
@@ -2,9 +2,8 @@ import { Gerente } from "../Usuario/Funcionario/Gerente/Gerente";
 import { RepositorioUsuario } from "../Repositorios/RepositorioUsuario";
 import { RepositorioProduto } from "../Repositorios/RepositorioProduto";
 
-class Interface{
-    
-}
+
 let g: Gerente = new Gerente("000000",12,"admin","gerente@admin.com","admin");
 let repositoriodeusuarios : RepositorioUsuario = new RepositorioUsuario;
 let repositoriodeprodutos: RepositorioProduto = new RepositorioProduto;
+
