47b4432046c5cb512ff02c048c50a61c8c116c8e
RaquelLopes8 <39920943+RaquelLopes8@users.noreply.github.com>
2018-06-16 17:03:52 -0300

    scm

---
+++ Produto/Repositorios/RepositorioUsuario.ts
@@ -0,0 +1,27 @@
+import { UsuarioAbstract } from "../../Usuario/UsuarioAbstract";
+
+export class RepositorioUsuario {
+    private repositorio : Map<string,UsuarioAbstract>;
+
+    public constructor(){
+        this.repositorio = new Map<string,UsuarioAbstract>()
+    }
+    public adicionarUsuario(email:string, usuario: UsuarioAbstract) : boolean{
+        if (this.repositorio.has(email)){
+            throw "Usuário de email: " + email + "já existe"
+        }
+        this.repositorio.set(email, usuario);
+        return true; 
+    }
+    public removerUsuario(email:string){
+        if (!this.repositorio.delete(email))
+            throw "Usuário de email: " + email + "não existe"
+    }
+    public retornarUsuario(email:string) : UsuarioAbstract{
+        let usuario = this.repositorio.get(email);
+        if (!usuario)
+            throw "Usuário de email: " + email + "não existe"
+        return usuario;
+    }
+
+}
---
+++ Repositorios/RepositorioProduto.ts
@@ -0,0 +1,28 @@
+import { ProdutoAbstract } from '../Produto/ProdutoAbstract';
+
+export class RepositorioProduto{
+    private repositorioP : Map<string,ProdutoAbstract>;
+
+    public constructor(){
+        this.repositorioP = new Map<string,ProdutoAbstract>();
+    }
+
+    public adicionarProduto(codigo:string, produto:ProdutoAbstract) : boolean{
+        if (this.repositorioP.has(codigo)){
+            throw "Codigo de produto: " + codigo + "já existe"
+        }
+        this.repositorioP.set(codigo, produto);
+        return true;
+    }
+    public removerProduto(codigo: string){
+        if (!this.repositorioP.delete(codigo))
+            throw "Codigo de produto: " + codigo + "não existe"    
+    }
+    public retornarProduto(codigo: string): ProdutoAbstract|undefined{
+        let produto = this.repositorioP.get(codigo);
+        if (!produto)
+            throw "Codigo de produto: " + codigo + "não existe"
+        return produto;
+       
+    }  
+}
