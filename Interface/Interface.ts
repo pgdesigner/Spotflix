6794fad3dcd08243d8070007ecdf033209205d97
pgdesigner <39765554+pgdesigner@users.noreply.github.com>
2018-06-17 16:18:28 -0300

    a

--- Interface/Interface.ts
+++ Interface/Interface.ts
@@ -8,3 +8,4 @@ class Interface{
 let g: Gerente = new Gerente("000000",12,"admin","gerente@admin.com","admin");
 let repositoriodeusuarios : RepositorioUsuario = new RepositorioUsuario;
 let repositoriodeprodutos: RepositorioProduto = new RepositorioProduto;
+
